6de58d348e507072e35a77f0a069f5f48fb7d9e9
chenyaojie <chenyaojie@homeking365.com>
2019-07-10 10:53:03 +0800

    测试修改

--- Runner/__init__.py
+++
@@ -1,5 +0,0 @@
-""" 
-@author: chenyj
-@file: __init__.py.py 
-@time: 2019/7/10 10:16 
-"""
--- Runner/run_all.py
+++ Runner/run_all.py
@@ -4,8 +4,7 @@ from HTMLTestRunner_cn import HTMLTestRunner
 from Base.sentMail import send_mail, new_file
 import os
 import getcwd
-import sys
-sys.path.append('../')
+
 
 test_dir = os.path.join(getcwd.get_cwd(), 'TestCases')
 discovery = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
@@ -18,5 +17,5 @@ if __name__ == '__main__':
         runner = HTMLTestRunner(stream=f, title='测试报告', description='测试结果')
         runner.run(discovery)
     f.close()
-    new_report = new_file(report_dir)
-    send_mail(new_report)
+    # new_report = new_file(report_dir)
+    # send_mail(new_report)
